f5caa721e443f70e9ec270adfcad61b0e94a191d
Tony K. <tony-koke@outlook.com>
2024-03-13 21:48:02 +0000

    S-1 Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div className="h-screen w-full text-white flex flex-col justify-start items-center gap-8 pt-40">
-      <h1 className="text-4xl font-bold mb-2">Welcome to Spotify Review</h1>
-      <div className="text-center text-xl">
-        <p>1. Click on Authorise Access</p>
+      <h1 className="text-4xl font-bold mb-2">Welcome to <span className="text-green-600">Spotify</span> Review</h1>
+      <div className="flex flex-col gap-2 text-center text-xl">
+        <p>1. Click on <span className="text-green-600">Authorise Access</span></p>
         <p>2. Get your review</p>
         <p>Click on Help for more</p>
       </div>
