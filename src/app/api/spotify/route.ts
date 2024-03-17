45cd39d30339c0e9038b042651ad430ff372a62a
Tony K. <tony-koke@outlook.com>
2024-03-17 11:41:00 +0000

    api update

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -1,9 +1,15 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
 
+export interface NextResponse<T = any> {
+  status: number;
+  data: T | null;
+  error?: AxiosError<any> | undefined;
+}
+
 export async function GET<T>(
   accessToken: any,
   url: string,
-): Promise<any> {
+): Promise<NextResponse<T>> {
   try {
     const response: AxiosResponse<T> = await axios({
       method: "get",
@@ -14,10 +20,12 @@ export async function GET<T>(
     });
 
     return {
-      data: response,
+      status: response.status, 
+      data: response.data,
     };
   } catch (error) {
     return {
+      status: (error as AxiosError).response?.status || 500,
       data: null,
       error: error as AxiosError<any>,
     };
