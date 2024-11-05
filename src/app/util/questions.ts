d81d07a8e1fb0f48bd03fc76e4f5b6f9edd16fd7
Tony K. <tony-koke@outlook.com>
2024-11-05 17:24:13 +0000

    SRU-3 Help Page Beginning Of Update

--- src/app/util/questions.ts
+++ src/app/util/questions.ts
@@ -1,22 +1,27 @@
 export const Questions = [
   {
-    question: "Reason for Spotify Review",
+    question: "Disclaimer",
     answer:
-      "The Spotify Review platform allows users to gain insights into their Spotify usage, including their favorite songs, artists, and playlists. It provides a comprehensive overview of their listening habits, helping users discover new music and understand their preferences better.",
+      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
   },
   {
-    question: "How to use platform",
+    question: "Creator",
     answer:
-      "To use the Spotify Review platform, simply visit the website and log in with your Spotify account. Once logged in, you'll have access to various features such as viewing your top tracks, artists, and genres, as well as personalized recommendations based on your listening history.",
+      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
   },
   {
-    question: "Details on the authentication",
+    question: "How authorise access works",
     answer:
-      "Authentication on Spotify Review is done securely through Spotify's OAuth authentication protocol. When you log in, you'll be redirected to the Spotify website to grant access to your account. This ensures that your account credentials are never shared with third-party applications and that your privacy and security are maintained.",
+      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
   },
   {
-    question: "More information",
+    question: "Purpose",
     answer:
-      "For more information about Spotify Review, including FAQs, troubleshooting tips, and contact details for support, please visit our Help Center. Our team is available to assist you with any questions or concerns you may have about the platform.",
+      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
+  },
+  {
+    question: "Remove Access",
+    answer:
+      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
   },
 ];
