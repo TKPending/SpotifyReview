1e7f60287008c6437dd2aac05778f373204e39df
Tony K. <tony-koke@outlook.com>
2024-11-06 15:44:18 +0000

    SRU-3 Updated Help Page Text

--- src/app/util/questions.ts
+++ src/app/util/questions.ts
@@ -2,26 +2,26 @@ export const Questions = [
   {
     question: "Disclaimer",
     answer:
-      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
+      "This project is a personal creation by Tony Koke, developed independently of Spotify. Information displayed on this site is fetched directly from Spotify's API but is never stored on external servers—all data remains in your local storage.",
   },
   {
-    question: "Creator",
+    question: "About the Project",
     answer:
-      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
+      "This was one of my early projects, which I later revisited to enhance. Initially a static site, it’s now a dynamic application designed to offer easy, streamlined access to insights about my Spotify listening habits. The goal was to provide an intuitive way to answer common questions about what I'm listening to and to share that with others.",
   },
   {
-    question: "How authorise access works",
+    question: "How Authorization Works",
     answer:
-      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
+      "When you click 'Authorize,' you’re redirected to Spotify’s site to grant this application permission to access your Spotify data. With this authorization, the site displays your Spotify profile, favorite artists, top songs, and recently played tracks. Importantly, this data is only accessed on your device and stored in your session storage. I do not have access to or store any of this information externally.",
   },
   {
     question: "Purpose",
     answer:
-      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
+      "I built this project to explore and present my Spotify listening data—favorite artists, top songs, and recent plays. It also provided a valuable opportunity to work with APIs and present data in a clear, user-friendly format.",
   },
   {
-    question: "Remove Access",
+    question: "Removing Access",
     answer:
-      "balfdsaf sdafadf afdsfasd fas dfsadf asdf adsf asf asdf asdf a fda sf asdf ",
+      "If you choose to remove access, this will delete the access token from session storage, effectively revoking this site’s permission to access your Spotify data.",
   },
 ];
