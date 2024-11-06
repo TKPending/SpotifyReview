948c367db1dab1df75ab03184cdac9f12a50a7bf
Tony K. <tony-koke@outlook.com>
2024-11-06 15:25:14 +0000

    SRU-6 Remove Access Button

--- src/app/components/RemoveAccessButton.tsx
+++ src/app/components/RemoveAccessButton.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const RemoveAccessButton = () => {
+  const router = useRouter();
   const [popup, setPopup] = useState<boolean>(false);
 
   const confirmRemoveAccess = () => {
@@ -9,8 +11,13 @@ const RemoveAccessButton = () => {
 
   const handleRemoveAccess = (option: boolean) => {
     if (option) {
-        // Remove Access
-        return;
+        const itemsToRemove: string[] = ["review_stored", "access_token", "code_verifier", "refresh_token", "user", "favouriteSongs", "favouriteArtists", "recentlyPlayed", "review_stored"];
+
+        itemsToRemove.forEach((item: string) => {
+            sessionStorage.removeItem(item);
+        });
+
+        router.push("/");
     }
 
     setPopup(false);
@@ -18,17 +25,16 @@ const RemoveAccessButton = () => {
 
   return (
     <div
-      onClick={confirmRemoveAccess}
       className="flex items-center justify-center absolute top-5 right-10"
     >
-      <p className="text-white font-semibold p-4 rounded-lg bg-green-600 hover:bg-green-800 transition duration-200 hover:cursor-pointer">
+      <p onClick={confirmRemoveAccess} className="text-white font-semibold p-4 rounded-lg bg-green-600 hover:bg-green-800 transition duration-200 hover:cursor-pointer">
         Remove Access
       </p>
 
       {popup && (
-        <div className="absolute top-16 text-white flex gap-6">
-          <p onClick={() => handleRemoveAccess(true)} className="hover:text-green-300 hover:cursor-pointer">Confirm</p>
-          <p onClick={() => handleRemoveAccess(false)} className="hover:text-red-200 hover:cursor-pointer">Decline</p>
+        <div className="absolute top-16 text-white flex gap-6 transition duration-600">
+          <p onClick={() => handleRemoveAccess(true)} className="hover:text-green-300 hover:cursor-pointer hover:underline transition duration-200">Confirm</p>
+          <p onClick={() => handleRemoveAccess(false)} className="hover:text-red-200 hover:cursor-pointer hover:underline transition duration-200">Decline</p>
         </div>
       )}
     </div>
