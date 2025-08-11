8f58d61cb25e55d78ebe800ca1c7e5ce7190d3e4
TKPending <tony-koke@outlook.com>
2025-08-11 19:54:41 +0100

    Review Content Update

--- src/app/containers/review/ReviewContentContainer.tsx
+++ src/app/containers/review/ReviewContentContainer.tsx
@@ -1,24 +1,19 @@
+import { Favourites, GlobalValues } from "@/app/global";
 import { useState, useEffect } from "react";
-import Fetching from "@/app/components/Fetching";
-import ReviewHeader from "./components/ReviewHeader";
-import ReviewArtists from "./container/ReviewArtists";
-import ReviewSongs from "./container/ReviewSongs";
-import { getContentFromStorage } from "@/app/util/sessionStorage/getContentFromStorage";
+import ReviewContent from "./ReviewContent";
 import SpotifyClient from "@/app/api/spotify/client/SpotifyClient";
+import { getContentFromStorage } from "@/app/util/sessionStorage/getContentFromStorage";
+import { setSessionStorage } from "@/app/util/sessionStorage/setSessionStorage";
+import { fetchFavouriteArtists } from "@/app/api/spotify/data/util/fetchFavouriteArtists";
+import { fetchFavouriteSongs } from "@/app/api/spotify/data/util/fetchFavouriteSongs";
+import { fetchRecentlyPlayed } from "@/app/api/spotify/data/util/fetchRecentlyPlayed";
+import { isErrorType } from "@/app/util/isErrorType";
 import {
   ReviewInterface,
   ArtistType,
   SongType,
   ErrorType,
 } from "@/app/types/ReviewTypes";
-import { setSessionStorage } from "@/app/util/sessionStorage/setSessionStorage";
-import { isErrorType } from "@/app/util/isErrorType";
-import ReviewError from "@/app/components/ReviewError";
-import SeeMoreButton from "./components/buttons/SeeMoreButton";
-import { Favourites, GlobalValues } from "@/app/global";
-import { fetchFavouriteArtists } from "@/app/api/spotify/data/util/fetchFavouriteArtists";
-import { fetchFavouriteSongs } from "@/app/api/spotify/data/util/fetchFavouriteSongs";
-import { fetchRecentlyPlayed } from "@/app/api/spotify/data/util/fetchRecentlyPlayed";
 
 type Props = {
   selectedOption: number;
@@ -125,52 +120,15 @@ const ReviewContentContainer = ({ selectedOption }: Props) => {
   }, [selectedOption, isFetching]);
 
   return (
-    <div className="w-full sm:w-4/5 flex items-center justify-center">
-      <div className="bg-green-600 h-3/4 w-[80%] rounded-lg shadow-xs shadow-gray-300 p-6 overflow-hidden">
-        {isLoading && <Fetching />}
-        {isError && <ReviewError text="There is an error" />}
-        {!isLoading && review && (
-          <div className="h-full flex flex-col gap-4">
-            <ReviewHeader
-              title={review.title}
-              description={review.description}
-              contentType={selectedOption}
-              handleRefresh={handleRefreshSongs}
-            />
-            <div className="flex flex-col gap-4 overflow-y-auto">
-              {review.content.map(
-                (item: ArtistType | SongType, index: number) => (
-                  <div key={index}>
-                    {selectedOption === Favourites.ARTISTS ? (
-                      <ReviewArtists
-                        ranking={index + 1}
-                        artistItem={item as ArtistType}
-                      />
-                    ) : (
-                      <ReviewSongs
-                        ranking={index + 1}
-                        type={
-                          selectedOption === Favourites.SONGS
-                            ? "song"
-                            : "recent"
-                        }
-                        song={item as SongType}
-                      />
-                    )}
-                  </div>
-                )
-              )}
-
-              {seeMoreVisible[selectedOption] &&
-                review.content.length !== GlobalValues.MAX_SONGS &&
-                review.content.length !== GlobalValues.MAX_RECENT && (
-                  <SeeMoreButton onClick={handleSeeMore} />
-                )}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+    <ReviewContent
+      isLoading={isLoading}
+      isError={isError}
+      review={review}
+      selectedOption={selectedOption}
+      seeMoreVisible={seeMoreVisible}
+      handleRefreshSongs={handleRefreshSongs}
+      handleSeeMore={handleSeeMore}
+    />
   );
 };
 
